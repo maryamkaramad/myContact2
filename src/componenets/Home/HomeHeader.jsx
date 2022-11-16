54c20a896676336e390443397f81605aa9a078cb
maryamkaramad <maryamkaramad1373@gmail.com>
2022-11-16 13:22:18 +0330

    fixbug header

--- src/componenets/Home/HomeHeader.jsx
+++ src/componenets/Home/HomeHeader.jsx
@@ -8,15 +8,24 @@ const HomeHeader = () => {
   return (
     <Grid
       container
-      display={"flex"}
-      justifyContent={"space-around"}
+      xs={12}
+      justifyContent={"center"}
       alignItems={"center"}
+      gap={2}
+      md={12}
+      sx={{ marginBottom: { xs: 2 } }}
     >
-      <Typography m={2} variant={"h6"} color={"GrayText"}>
-        <ContactPageIcon sx={{ fontSize: 50 }} />
-        {contacts.length} contacts
-      </Typography>
-      <Grid>
+      <Grid item continer xs={12} md={6} justifyContent={"flex-start"}
+        alignItems={"center"}>
+        <Grid item><Typography m={2} variant={"h6"} color={"GrayText"}></Typography></Grid>
+        <Grid item> <ContactPageIcon sx={{ fontSize: 50 }} />
+          {contacts.length} contacts</Grid>
+
+
+      </Grid>
+
+      <Grid item container xs={12} md={6} justifyContent={"flex-end"}
+        alignItems={"center"}>
         <Link to={"/addcontact"}>
           <Button variant="contained" color="success">
             add contact
